8db1257ffac2837caaa8521482cf7f97e5f68004
ElijahKyule <elijahkyule36@gmail.com>
2019-10-23 12:18:02 +0300

    first commit

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header>
+		<nav>
+			<div class = main-wrapper>
+				<ul>
+					<li><a href="index.php">HOME</a></li>
+				</ul>
+				<div class="nav-login">
+					<form>
+						<input type="text" name="uid" placeholder="Username/e-mail">
+						<input type="password" name="pwd" placeholder="password">
+						<button type="submit" name="submit">Login</button>
+					</form>
+					<a href="signup.php">Signup</a>
+				</div>
+				
+			</div>
+		</nav>
+		
+	</header>
